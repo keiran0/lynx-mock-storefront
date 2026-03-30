16a693f7ee74dc00b38bde0b35fd5c4c8e02e20a
Keiran <keinn2210@gmail.com>
2026-03-30 17:06:30 +0800

    Add TopBar component and update App styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Nav from './components/Nav'
 import Content from './components/Content'
+import TopBar from './components/TopBar'
 
 export function App() {
 
   return (
-    <view>
+    <view className="flex justify-between flex-col h-full pb-14 bg-gray-600 ">
+      <TopBar/>
       <Content/>
       <Nav/>
     </view>
---
+++ src/components/TopBar.tsx
@@ -0,0 +1,5 @@
+export default function TopBar() {
+    return <view className="bg-blue-400 h-14">
+        <text>Placeholder stuff</text>
+    </view>
+}
